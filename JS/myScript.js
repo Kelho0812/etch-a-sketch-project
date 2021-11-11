feba27eb67c570dd2f7b332573357b9d11ae0485
Kelho0812 <Jorge0812@gmail.com>
2021-11-11 19:05:31 +0000

    Added random color & hover effect on the grid

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,16 +1,26 @@
+function colorPicker() {
+    let colorsArray = ['#edd3c4','#C8ADC0','#7765E3','#3B60E4','#080708',]
+    let randomColor =
+        colorsArray[Math.floor(Math.random() * colorsArray.length)];
+    return randomColor;}
+
 function createGrid() {
     const gridContainer = document.querySelector(".gridContainer");
-
     for (let i = 0; i < 256; i++) {
         const gridSquares = document.createElement("div");
         gridSquares.classList.add("gridSquares");
         gridSquares.style.width = "60px";
         gridSquares.style.height = "60px";
-        gridSquares.textContent = "Test";
+        // gridSquares.textContent = "Test";
         gridContainer.appendChild(gridSquares);
+        const gridzz = gridContainer.querySelectorAll('div')
+        gridzz.forEach((div) => {
+            // and for each one we add a 'click' listener
+            div.addEventListener("mouseenter", () => {
+                div.style.backgroundColor = colorPicker();
+            });
+        });
     }
 }
-
-
 
 window.onload = createGrid();
