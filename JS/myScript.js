e8a1137ecc701ec3692aefd1f5e4215aa5a75883
Kelho0812 <Jorge0812@gmail.com>
2021-11-23 15:17:11 +0000

    Added a Value Slider

--- JS/myScript.js
+++ JS/myScript.js
@@ -1,3 +1,21 @@
+const slider = document.getElementById("myRange");
+let output = document.getElementById("demo");
+const gridContainer = document.querySelector(".gridContainer");
+
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function () {
+    output.innerHTML = this.value;
+};
+
+let button = document.querySelector(".button");
+button.addEventListener("click", () => {
+    // clearGrid() //ENCONTRAR MANEIRA DE REMOVER TODOS OS DIVS ANTES DE CRIAR A GRID NOVA
+    createGrid()
+    paintGrid() 
+});
+
 function colorPicker() {
     let colorsArray = ["#edd3c4", "#C8ADC0", "#7765E3", "#3B60E4", "#080708"];
     let randomColor =
@@ -5,25 +23,40 @@ function colorPicker() {
     return randomColor;
 }
 
+function createGrid(x) {
+    let sliderValue = slider.value;
+    console.log(sliderValue);
+    gridContainer.style.gridTemplateRows =
+        'repeat(' + sliderValue + "," + 600 / sliderValue + 'px)';
+    gridContainer.style.gridTemplateColumns =
+    'repeat(' + sliderValue + "," + 600 / sliderValue + "px)";
 
-
-function createGrid() {
-    const gridContainer = document.querySelector(".gridContainer");
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < (sliderValue * sliderValue); i++) {
         const gridSquares = document.createElement("div");
         gridSquares.classList.add("gridSquares");
-        gridSquares.style.width = "60px";
-        gridSquares.style.height = "60px";
+        gridSquares.setAttribute("id","gridSquares");
+        gridSquares.style.width = 600 / sliderValue + "px";
+        gridSquares.style.height = 600 / sliderValue + "px";
         // gridSquares.textContent = "Test";
         gridContainer.appendChild(gridSquares);
-        const gridzz = gridContainer.querySelectorAll("div");
-        gridzz.forEach((div) => {
-            // and for each one we add a 'click' listener
-            div.addEventListener("mouseenter", () => {
-                div.style.backgroundColor = colorPicker();
-            });
-        });
     }
 }
 
-window.onload = createGrid();
+
+
+function paintGrid() {
+    const gridzz = gridContainer.querySelectorAll("div");
+    gridzz.forEach((div) => {
+        // and for each one we add a 'click' listener
+        div.addEventListener("mouseenter", () => {
+            div.style.backgroundColor = colorPicker();
+        });
+    });
+}
+
+// function clearGrid() {
+//     var elem = document.getElementById('gridSquares');
+//     elem.parentNode.removeChild(elem);
+//     return false;
+// }
+// (window.onload = createGrid()), paintGrid();
